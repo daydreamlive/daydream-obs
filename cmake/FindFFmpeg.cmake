6cfd7e28b9828e4029eae20917678c3d4cb534cd
junhyr <junhyoung@livepeer.org>
2026-01-26 18:05:22 +0900

    chore: format

--- cmake/FindFFmpeg.cmake
+++ cmake/FindFFmpeg.cmake
@@ -15,61 +15,47 @@ if(WIN32)
   # Windows: Look in obs-deps
   file(GLOB _OBS_DEPS_DIRS "${CMAKE_SOURCE_DIR}/.deps/obs-deps-*-x64")
   list(APPEND _FFMPEG_SEARCH_PATHS ${_OBS_DEPS_DIRS})
-  
-  find_path(AVCODEC_INCLUDE_DIR
-    NAMES libavcodec/avcodec.h
-    PATHS ${_FFMPEG_SEARCH_PATHS}
-    PATH_SUFFIXES include
-  )
-  
-  find_library(AVCODEC_LIBRARY
+
+  find_path(AVCODEC_INCLUDE_DIR NAMES libavcodec/avcodec.h PATHS ${_FFMPEG_SEARCH_PATHS} PATH_SUFFIXES include)
+
+  find_library(
+    AVCODEC_LIBRARY
     NAMES avcodec avcodec-61 avcodec-60 avcodec-59 avcodec-58
     PATHS ${_FFMPEG_SEARCH_PATHS}
     PATH_SUFFIXES lib bin
   )
-  
-  find_library(AVUTIL_LIBRARY
+
+  find_library(
+    AVUTIL_LIBRARY
     NAMES avutil avutil-59 avutil-58 avutil-57 avutil-56
     PATHS ${_FFMPEG_SEARCH_PATHS}
     PATH_SUFFIXES lib bin
   )
-  
-  find_library(SWSCALE_LIBRARY
+
+  find_library(
+    SWSCALE_LIBRARY
     NAMES swscale swscale-8 swscale-7 swscale-6 swscale-5
     PATHS ${_FFMPEG_SEARCH_PATHS}
     PATH_SUFFIXES lib bin
   )
-  
 elseif(APPLE)
   # macOS: Look in obs-deps or homebrew
-  file(GLOB _OBS_DEPS_DIRS "${CMAKE_SOURCE_DIR}/.deps/obs-deps-*-universal" "${CMAKE_SOURCE_DIR}/.deps/obs-deps-*-arm64" "${CMAKE_SOURCE_DIR}/.deps/obs-deps-*-x86_64")
+  file(
+    GLOB _OBS_DEPS_DIRS
+    "${CMAKE_SOURCE_DIR}/.deps/obs-deps-*-universal"
+    "${CMAKE_SOURCE_DIR}/.deps/obs-deps-*-arm64"
+    "${CMAKE_SOURCE_DIR}/.deps/obs-deps-*-x86_64"
+  )
   list(APPEND _FFMPEG_SEARCH_PATHS ${_OBS_DEPS_DIRS})
   list(APPEND _FFMPEG_SEARCH_PATHS "/opt/homebrew" "/usr/local")
-  
-  find_path(AVCODEC_INCLUDE_DIR
-    NAMES libavcodec/avcodec.h
-    PATHS ${_FFMPEG_SEARCH_PATHS}
-    PATH_SUFFIXES include
-  )
-  
-  find_library(AVCODEC_LIBRARY
-    NAMES avcodec
-    PATHS ${_FFMPEG_SEARCH_PATHS}
-    PATH_SUFFIXES lib
-  )
-  
-  find_library(AVUTIL_LIBRARY
-    NAMES avutil
-    PATHS ${_FFMPEG_SEARCH_PATHS}
-    PATH_SUFFIXES lib
-  )
-  
-  find_library(SWSCALE_LIBRARY
-    NAMES swscale
-    PATHS ${_FFMPEG_SEARCH_PATHS}
-    PATH_SUFFIXES lib
-  )
-  
+
+  find_path(AVCODEC_INCLUDE_DIR NAMES libavcodec/avcodec.h PATHS ${_FFMPEG_SEARCH_PATHS} PATH_SUFFIXES include)
+
+  find_library(AVCODEC_LIBRARY NAMES avcodec PATHS ${_FFMPEG_SEARCH_PATHS} PATH_SUFFIXES lib)
+
+  find_library(AVUTIL_LIBRARY NAMES avutil PATHS ${_FFMPEG_SEARCH_PATHS} PATH_SUFFIXES lib)
+
+  find_library(SWSCALE_LIBRARY NAMES swscale PATHS ${_FFMPEG_SEARCH_PATHS} PATH_SUFFIXES lib)
 else()
   # Linux: Use pkg-config
   find_package(PkgConfig QUIET)
@@ -78,26 +64,30 @@ else()
     pkg_check_modules(PC_AVUTIL QUIET libavutil)
     pkg_check_modules(PC_SWSCALE QUIET libswscale)
   endif()
-  
-  find_path(AVCODEC_INCLUDE_DIR
+
+  find_path(
+    AVCODEC_INCLUDE_DIR
     NAMES libavcodec/avcodec.h
     HINTS ${PC_AVCODEC_INCLUDEDIR} ${PC_AVCODEC_INCLUDE_DIRS}
     PATHS /usr/include /usr/local/include
   )
-  
-  find_library(AVCODEC_LIBRARY
+
+  find_library(
+    AVCODEC_LIBRARY
     NAMES avcodec
     HINTS ${PC_AVCODEC_LIBDIR} ${PC_AVCODEC_LIBRARY_DIRS}
     PATHS /usr/lib /usr/lib/x86_64-linux-gnu /usr/local/lib
   )
-  
-  find_library(AVUTIL_LIBRARY
+
+  find_library(
+    AVUTIL_LIBRARY
     NAMES avutil
     HINTS ${PC_AVUTIL_LIBDIR} ${PC_AVUTIL_LIBRARY_DIRS}
     PATHS /usr/lib /usr/lib/x86_64-linux-gnu /usr/local/lib
   )
-  
-  find_library(SWSCALE_LIBRARY
+
+  find_library(
+    SWSCALE_LIBRARY
     NAMES swscale
     HINTS ${PC_SWSCALE_LIBDIR} ${PC_SWSCALE_LIBRARY_DIRS}
     PATHS /usr/lib /usr/lib/x86_64-linux-gnu /usr/local/lib
@@ -111,35 +101,36 @@ message(STATUS "AVCODEC_LIBRARY: ${AVCODEC_LIBRARY}")
 message(STATUS "AVUTIL_LIBRARY: ${AVUTIL_LIBRARY}")
 message(STATUS "SWSCALE_LIBRARY: ${SWSCALE_LIBRARY}")
 
-find_package_handle_standard_args(FFmpeg
+find_package_handle_standard_args(
+  FFmpeg
   REQUIRED_VARS AVCODEC_LIBRARY AVUTIL_LIBRARY SWSCALE_LIBRARY AVCODEC_INCLUDE_DIR
 )
 
 if(FFmpeg_FOUND)
   set(FFmpeg_INCLUDE_DIRS ${AVCODEC_INCLUDE_DIR})
   set(FFmpeg_LIBRARIES ${AVCODEC_LIBRARY} ${AVUTIL_LIBRARY} ${SWSCALE_LIBRARY})
-  
+
   if(NOT TARGET FFmpeg::avcodec)
     add_library(FFmpeg::avcodec UNKNOWN IMPORTED)
-    set_target_properties(FFmpeg::avcodec PROPERTIES
-      IMPORTED_LOCATION "${AVCODEC_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${AVCODEC_INCLUDE_DIR}"
+    set_target_properties(
+      FFmpeg::avcodec
+      PROPERTIES IMPORTED_LOCATION "${AVCODEC_LIBRARY}" INTERFACE_INCLUDE_DIRECTORIES "${AVCODEC_INCLUDE_DIR}"
     )
   endif()
-  
+
   if(NOT TARGET FFmpeg::avutil)
     add_library(FFmpeg::avutil UNKNOWN IMPORTED)
-    set_target_properties(FFmpeg::avutil PROPERTIES
-      IMPORTED_LOCATION "${AVUTIL_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${AVCODEC_INCLUDE_DIR}"
+    set_target_properties(
+      FFmpeg::avutil
+      PROPERTIES IMPORTED_LOCATION "${AVUTIL_LIBRARY}" INTERFACE_INCLUDE_DIRECTORIES "${AVCODEC_INCLUDE_DIR}"
     )
   endif()
-  
+
   if(NOT TARGET FFmpeg::swscale)
     add_library(FFmpeg::swscale UNKNOWN IMPORTED)
-    set_target_properties(FFmpeg::swscale PROPERTIES
-      IMPORTED_LOCATION "${SWSCALE_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${AVCODEC_INCLUDE_DIR}"
+    set_target_properties(
+      FFmpeg::swscale
+      PROPERTIES IMPORTED_LOCATION "${SWSCALE_LIBRARY}" INTERFACE_INCLUDE_DIRECTORIES "${AVCODEC_INCLUDE_DIR}"
     )
   endif()
 endif()
